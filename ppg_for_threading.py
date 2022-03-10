549adf5cbb3f974e6b10c3d7ffec492228405cbd
Jacob Estep <jacobwestep@gmail.com>
2022-03-09 23:24:17 -0800

    Added in game function as thread

--- ppg_for_threading.py
+++ ppg_for_threading.py
@@ -2,11 +2,15 @@
 import serial
 import time
 from scipy import signal
-import keyboard
-import csv
-import datetime
 import numpy as np
 import threading
+from rpi_lcd import LCD
+import random
+import RPi.GPIO as GPIO
+
+## SETUP ##
+GPIO.setwarnings(False) #hide warnings
+GPIO.setmode(GPIO.BCM) #use BCM pin numbering
 
 ## GLOBAL VARS ##
 global_time_data = []
@@ -17,6 +21,12 @@ mem_score1 = 0
 mem_score2 = 0
 mem_score3 = 0
 ppg_feature = 0
+
+## WAIT FUNCTION ##
+def wait(s):
+    start = time.time()
+    while time.time()-start < s:
+        pass
 
 ## MAIN FUNCTION ##
 def main_task():
@@ -41,8 +51,391 @@ def send_to_cloud(react, mole1, mem1, mem2, mem3, ppgF):
 
 ## GAME ## TODO
 def play_game(lock):
-    #do stuff
-    pass
+    global avg_reaction_time
+    global mole_score
+    global mem_score1
+    global mem_score2
+    global mem_score3
+
+    pins = [[11, 18],[9, 15],[7, 14],[8, 4],[22,27]] #LED, Button format; Green, Yellow, Blue, White order
+
+    for i in range(5): #setup LED pins
+        GPIO.setup(pins[i][0], GPIO.OUT)
+    for i in range(5): #setup button pins
+        GPIO.setup(pins[i][1], GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+
+    input("Press ENTER to play")
+
+    ######################
+    ## Wack-a-mole Game ##
+    ######################
+
+    reaction_times = []
+    runTimes = 50
+    for molePopup in range(runTimes):
+        speed = random.randint(4, 12) # randomise speed
+        randomNumber = random.randint(0, 4) # choose random LED
+        GPIO.output(pins[randomNumber][0],GPIO.HIGH) # light LED
+        light_time = time.time()
+        for i in range(speed):
+			# Check if the correct button is pressed
+            if GPIO.input(pins[randomNumber][1]) == True:
+                reaction_time = time.time() - light_time
+                reaction_times.append(reaction_time)
+                score = score + 1
+				# lcd.text(str(score), 2)
+                GPIO.output(pins[randomNumber][0],GPIO.LOW)
+                break
+            wait(0.1)
+        GPIO.output(pins[randomNumber][0],GPIO.LOW) # turn off LED
+        wait(0.2) # + speed by - num of times button is checked for press, speed=speed-1
+    lock.acquire()
+    avg_reaction_time = np.mean(reaction_times)
+    mole_score = score
+    lock.release()
+    print("Average Reaction Time:", avg_reaction_time)
+    print("Game over. You scored ", score, "out of ", runTimes)
+    wait(2)
+    input("Press ENTER to play memory game")
+
+    #################
+    ## Memory Game ##
+    #################
+
+    # practice round
+    results_1 = []
+    randomNumber1_1 = random.randint(0, 4)
+    randomNumber1_2 = random.randint(0, 4)
+    randomNumber1_3 = random.randint(0, 4)
+    solution_1 = [randomNumber1_1, randomNumber1_2, randomNumber1_3]
+    print("Practice Round")
+    GPIO.output(pins[randomNumber1_1][0], GPIO.HIGH)
+    wait(1)
+    GPIO.output(pins[randomNumber1_1][0], GPIO.LOW)
+    wait(1)
+    GPIO.output(pins[randomNumber1_2][0], GPIO.HIGH)
+    wait(1)
+    GPIO.output(pins[randomNumber1_2][0], GPIO.LOW)
+    wait(1)
+    GPIO.output(pins[randomNumber1_3][0], GPIO.HIGH)
+    wait(1)
+    GPIO.output(pins[randomNumber1_3][0], GPIO.LOW)
+    wait(1)
+
+    count = 0
+    while count<3:
+        if GPIO.input(pins[0][1]) == True:
+            results_1.append(0)
+            count = count + 1
+            print(count)
+            wait(0.45)
+        elif GPIO.input(pins[1][1]) == True:
+            results_1.append(1)
+            count = count + 1
+            print(count)
+            wait(0.45)
+        elif GPIO.input(pins[2][1]) == True:
+            results_1.append(2)
+            count = count + 1
+            print(count)
+            wait(0.45)
+        elif GPIO.input(pins[3][1]) == True:
+            results_1.append(3)
+            count = count + 1
+            print(count)
+            wait(0.45)
+        elif GPIO.input(pins[4][1]) == True:
+            results_1.append(4)
+            count = count + 1
+            print(count)
+            wait(0.45)
+    print("solution:", solution_1)
+    print("results:", results_1)
+    if results_1[0] == solution_1 [0]:
+        score_1 = score_1 +1
+    if results_1[1] == solution_1 [1]:
+        score_1 = score_1 +1
+    if results_1[2] == solution_1 [2]:
+        score_1 = score_1 +1
+    print("score:", score_1)
+	# lcd.text("Score:",1)
+	# lcd.text(str(score_1),2)
+    wait(2)
+	# lcd.text("Press ENTER to", 1)
+	# lcd.text("play round 1", 2)
+    input("Press ENTER to play round 1")
+
+    # round 1
+    score_2 = 0
+    solution_2 = []
+    randomNumber2_1 = random.randint(0, 4)
+    randomNumber2_2 = random.randint(0, 4)
+    randomNumber2_3 = random.randint(0, 4)
+    results_2 = [randomNumber2_1, randomNumber2_2, randomNumber2_3]
+    print("Round 1") #display round 1
+    # lcd.clear()
+    # lcd.text("Round 1", 1)
+    GPIO.output(pins[randomNumber2_1][0], GPIO.HIGH)
+    wait(1)
+    GPIO.output(pins[randomNumber2_1][0], GPIO.LOW)
+    wait(1)
+    GPIO.output(pins[randomNumber2_2][0], GPIO.HIGH)
+    wait(1)
+    GPIO.output(pins[randomNumber2_2][0], GPIO.LOW)
+    wait(1)
+    GPIO.output(pins[randomNumber2_3][0], GPIO.HIGH)
+    wait(1)
+    GPIO.output(pins[randomNumber2_3][0], GPIO.LOW)
+
+    count = 0
+    while count < 3:
+        if GPIO.input(pins[0][1]) == True:
+            results_2.append(0)
+            count = count + 1
+            print(count)
+            wait(0.45)
+        elif GPIO.input(pins[1][1]) == True:
+            results_2.append(1)
+            count = count + 1
+            print(count)
+            wait(0.45)
+        elif GPIO.input(pins[2][1]) == True:
+            results_2.append(2)
+            count = count + 1
+            print(count)
+            wait(0.45)
+        elif GPIO.input(pins[3][1]) == True:
+            results_2.append(3)
+            count = count + 1
+            print(count)
+            wait(0.45)
+        elif GPIO.input(pins[4][1]) == True:
+            results_2.append(4)
+            count = count + 1
+            print(count)
+            wait(0.45)
+
+    print("solution:", solution_2)
+    print("results:", results_2)
+
+    # Grading Results to get Score
+    if results_2[0] == solution_2[0]:
+        score_2 = score_2 + 1
+
+    if results_2[1] == solution_2[1]:
+        score_2 = score_2 + 1
+
+    if results_2[2] == solution_2[2]:
+        score_2 = score_2 + 1
+
+    print("score:", score_2)
+    # lcd.text("Score:", 1)
+    # lcd.text(str(score_2), 2)
+    wait(2)
+    # lcd.text("Press ENTER to", 1)
+    # lcd.text("play round 2", 2)
+    input("Press ENTER to play round 2")
+
+    # round 2
+    score_3 = 0
+    results_3 = []
+    randomNumber3_1 = random.randint(0, 4)
+    randomNumber3_2 = random.randint(0, 4)
+    randomNumber3_3 = random.randint(0, 4)
+    randomNumber3_4 = random.randint(0, 4)
+    randomNumber3_5 = random.randint(0, 4)
+    
+    solution_3 = [randomNumber3_1, randomNumber3_2, randomNumber3_3, randomNumber3_4, randomNumber3_5]
+    # lcd.clear()
+    # lcd.text("Round 2", 1)
+    print("Round 2")
+    GPIO.output(pins[randomNumber3_1][0], GPIO.HIGH)
+    wait(1)
+    GPIO.output(pins[randomNumber3_1][0], GPIO.LOW)
+    wait(1)
+    GPIO.output(pins[randomNumber3_2][0], GPIO.HIGH)
+    wait(1)
+    GPIO.output(pins[randomNumber3_2][0], GPIO.LOW)
+    wait(1)
+    GPIO.output(pins[randomNumber3_3][0], GPIO.HIGH)
+    wait(1)
+    GPIO.output(pins[randomNumber3_3][0], GPIO.LOW)
+    wait(1)
+    GPIO.output(pins[randomNumber3_4][0], GPIO.HIGH)
+    wait(1)
+    GPIO.output(pins[randomNumber3_4][0], GPIO.LOW)
+    wait(1)
+    GPIO.output(pins[randomNumber3_5][0], GPIO.HIGH)
+    wait(1)
+    GPIO.output(pins[randomNumber3_5][0], GPIO.LOW)
+
+    count = 0
+    while count < 5:
+        if GPIO.input(pins[0][1]) == True:
+            results_3.append(0)
+            count = count + 1
+            print(count)
+            wait(0.45)
+        elif GPIO.input(pins[1][1]) == True:
+            results_3.append(1)
+            count = count + 1
+            print(count)
+            wait(0.45)
+        elif GPIO.input(pins[2][1]) == True:
+            results_3.append(2)
+            count = count + 1
+            print(count)
+            wait(0.45)
+        elif GPIO.input(pins[3][1]) == True:
+            results_3.append(3)
+            count = count + 1
+            print(count)
+            wait(0.45)
+        elif GPIO.input(pins[4][1]) == True:
+            results_3.append(4)
+            count = count + 1
+            print(count)
+            wait(0.45)
+
+    print("solution:", solution_3)
+    print("results:", results_3)
+
+    # Grading Results to get Score
+    if results_3[0] == solution_3[0]:
+        score_3 = score_3 + 1
+
+    if results_3[1] == solution_3[1]:
+        score_3 = score_3 + 1
+
+    if results_3[2] == solution_3[2]:
+        score_3 = score_3 + 1
+
+    if results_3[3] == solution_3[3]:
+        score_3 = score_3 + 1
+
+    if results_3[4] == solution_3[4]:
+        score_3 = score_3 + 1
+    #
+    # lcd.text("Score:", 1)
+    # lcd.text(str(score_3), 2)
+    wait(2)
+
+    # lcd.text("Press ENTER to", 1)
+    # lcd.text("play round 3", 2)
+
+    input("Press ENTER to play round 3")
+
+    print("score:", score_3)
+
+    # Setup final game
+    score_4 = 0
+    results_4 = []
+    randomNumber4_1 = random.randint(0, 4)
+    randomNumber4_2 = random.randint(0, 4)
+    randomNumber4_3 = random.randint(0, 4)
+    randomNumber4_4 = random.randint(0, 4)
+    randomNumber4_5 = random.randint(0, 4)
+    randomNumber4_6 = random.randint(0, 4)
+    randomNumber4_7 = random.randint(0, 4)
+    randomNumber4_8 = random.randint(0, 4)
+    
+    solution_4 = [randomNumber4_1, randomNumber4_2, randomNumber4_3, randomNumber4_4, randomNumber4_5, randomNumber4_6, randomNumber4_7, randomNumber4_8]
+
+    # Display third game
+    # lcd.clear()
+    # lcd.text("Round 3", 1)
+    print("Round 3")
+    GPIO.output(pins[randomNumber4_1][0], GPIO.HIGH)
+    wait(1)
+    GPIO.output(pins[randomNumber4_1][0], GPIO.LOW)
+    wait(1)
+    GPIO.output(pins[randomNumber4_2][0], GPIO.HIGH)
+    wait(1)
+    GPIO.output(pins[randomNumber4_2][0], GPIO.LOW)
+    wait(1)
+    GPIO.output(pins[randomNumber4_3][0], GPIO.HIGH)
+    wait(1)
+    GPIO.output(pins[randomNumber4_3][0], GPIO.LOW)
+    wait(1)
+    GPIO.output(pins[randomNumber4_4][0], GPIO.HIGH)
+    wait(1)
+    GPIO.output(pins[randomNumber4_4][0], GPIO.LOW)
+    wait(1)
+    GPIO.output(pins[randomNumber4_5][0], GPIO.HIGH)
+    wait(1)
+    GPIO.output(pins[randomNumber4_5][0], GPIO.LOW)
+    wait(1)
+    GPIO.output(pins[randomNumber4_6][0], GPIO.HIGH)
+    wait(1)
+    GPIO.output(pins[randomNumber4_6][0], GPIO.LOW)
+    wait(1)
+    GPIO.output(pins[randomNumber4_7][0], GPIO.HIGH)
+    wait(1)
+    GPIO.output(pins[randomNumber4_7][0], GPIO.LOW)
+    wait(1)
+    GPIO.output(pins[randomNumber4_8][0], GPIO.HIGH)
+    wait(1)
+    GPIO.output(pins[randomNumber4_8][0], GPIO.LOW)
+    wait(1)
+
+    count = 0
+    while count < 8:
+        if GPIO.input(pins[0][1]) == True:
+            results_4.append(0)
+            count = count + 1
+            print(count)
+            wait(0.45)
+        elif GPIO.input(pins[1][1]) == True:
+            results_4.append(1)
+            count = count + 1
+            print(count)
+            wait(0.45)
+        elif GPIO.input(pins[2][1]) == True:
+            results_4.append(2)
+            count = count + 1
+            print(count)
+            wait(0.45)
+        elif GPIO.input(pins[3][1]) == True:
+            results_4.append(3)
+            count = count + 1
+            print(count)
+            wait(0.45)
+        elif GPIO.input(pins[4][1]) == True:
+            results_4.append(4)
+            count = count + 1
+            print(count)
+            wait(0.45)
+
+    print("solution:", solution_4)
+    print("results:", results_4)
+
+    # Grading Results to get Score
+    if results_4[0] == solution_4[0]:
+        score_4 = score_4 + 1
+    if results_4[1] == solution_4[1]:
+        score_4 = score_4 + 1
+    if results_4[2] == solution_4[2]:
+        score_4 = score_4 + 1
+    if results_4[3] == solution_4[3]:
+        score_4 = score_4 + 1
+    if results_4[4] == solution_4[4]:
+        score_4 = score_4 + 1
+    if results_4[5] == solution_4[5]:
+        score_4 = score_4 + 1
+    if results_4[6] == solution_4[6]:
+        score_4 = score_4 + 1
+    if results_4[7] == solution_4[7]:
+        score_4 = score_4 + 1
+    print("score:", score_4)
+
+    # lcd.text("Score:", 1)
+    # lcd.text(str(score_4), 2)
+
+    lock.aquire()
+    mem_score1 = score_2
+    mem_score2 = score_3
+    mem_score3 = score_4
+    lock.release()    
 
 ## SENSOR READING ##
 def ppg_reading(lock):
@@ -67,6 +460,7 @@ def ppg_reading(lock):
 def ppg_processing(lock):
     global global_time_data
     global global_hr_data
+    global ppg_feature
 
     heart_rates = []
 
@@ -97,3 +491,4 @@ def ppg_processing(lock):
 
     #feature extraction on new list of BPMs
     hr_array = np.array(heart_rates)
+    ppg_feature = np.std(a=hr_array)
